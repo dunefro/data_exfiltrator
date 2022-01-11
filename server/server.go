43c4c567e78bcffe0512c018f477bdbf631c546c
Vedant Pareek <dunefro@gmail.com>
2022-01-11 23:30:29 +0530

    Server init

--- server/server.go
+++ server/server.go
@@ -1,13 +1,61 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+)
 
 const (
 	connHost = "localhost"
-	connPort = 8080
+	// connPort = "8080"
 	connType = "tcp"
 )
 
 func main() {
-	fmt.Printf("Starting %s server on %s:%d", connType, connHost, connPort)
+
+	// to get the port
+	var connPort string
+	if len(os.Args) <= 1 {
+		fmt.Println("No port was specified, using default 8080")
+		connPort = "8080"
+	} else {
+		connPort = os.Args[1]
+	}
+
+	fmt.Printf("Starting %s server on %s:%s\n", connType, connHost, connPort)
+	conn, err := net.Listen(connType, connHost+":"+connPort)
+	if err != nil {
+		fmt.Println("Connection error", connHost+":"+connPort)
+		panic(err.Error())
+	}
+	defer conn.Close()
+
+	// running the loop for listening all the connections
+	fmt.Println("Listening ... ")
+	for {
+		// Start accepting the connections
+		client, err := conn.Accept()
+		if err != nil {
+			panic(err.Error())
+		}
+		fmt.Println("Client", client.RemoteAddr().String(), "connected")
+		go handleClientConnection(client)
+	}
+}
+
+func handleClientConnection(conn net.Conn) {
+	// handling buffer writes
+	// it take the connection and then creates the buffer
+	for {
+		buffer, err := bufio.NewReader(conn).ReadBytes('\n')
+		if err != nil {
+			fmt.Println("Client left")
+			conn.Close()
+			return
+		}
+		fmt.Print("Client message:", string(buffer[:]))
+	}
+
 }
