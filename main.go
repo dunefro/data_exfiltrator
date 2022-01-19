ae6cf2715931e76ef7b953c2a5e9fb6f44f6a070
Vedant Pareek <dunefro@gmail.com>
2022-01-19 21:56:34 +0530

    Added cobra cli for client

---
+++ main.go
@@ -0,0 +1,15 @@
+/*
+Copyright © 2022 Vedant Pareek <pareekvedant99@gmail.com>
+
+*/
+package main
+
+import "github.com/dunefro/data_exfiltrator/cmd"
+
+func main() {
+	cmd.Execute()
+	// info, _ := os.Stat("./client/sample_input.txt")
+	// mode := info.Mode()
+	// fmt.Println(mode.String()[1:4])
+	// fmt.Println(info.Mode())
+}
