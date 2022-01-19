943864884c01b0604743bb3d6e0d3fe413e4761b
Vedant Pareek <dunefro@gmail.com>
2022-01-19 23:34:31 +0530

    filtering

--- main.go
+++ main.go
@@ -8,8 +8,4 @@ import "github.com/dunefro/data_exfiltrator/cmd"
 
 func main() {
 	cmd.Execute()
-	// info, _ := os.Stat("./client/sample_input.txt")
-	// mode := info.Mode()
-	// fmt.Println(mode.String()[1:4])
-	// fmt.Println(info.Mode())
 }
