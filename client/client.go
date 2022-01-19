b1c333f6214254c37d4b1f9a7f65312518015d7a
Vedant Pareek <dunefro@gmail.com>
2022-01-19 22:09:15 +0530

    Modified error and code filtering

--- client/client.go
+++ client/client.go
@@ -11,10 +11,7 @@ const connType = "tcp"
 
 func checkFile(file string) error {
 	_, err := os.Stat(file)
-	if err != nil {
-		return err
-	}
-	return nil
+	return err
 	// check file permissions as well
 }
 
@@ -22,7 +19,7 @@ func ExfiltrateFile(fileName, connHost, connPort string) error {
 
 	// stat file
 	if checkFile(fileName) != nil {
-		return fmt.Errorf("FileNotFound: Not able to find the file %s.", fileName)
+		return fmt.Errorf("FileNotFound: Not able to find the file %s", fileName)
 	}
 
 	// check connection
@@ -53,58 +50,3 @@ func ExfiltrateFile(fileName, connHost, connPort string) error {
 	}
 	return nil
 }
-
-// func main() {
-// 	var connHost string
-// 	var connPort string
-// 	connHost, connPort = getHostandPort(os.Args)
-// 	fmt.Printf("Connecting %s:%s over %s\n", connHost, connPort, connType)
-
-// 	// dial will connect to the server
-// 	conn, err := net.Dial(connType, connHost+":"+connPort)
-// 	defer conn.Close()
-// 	if err != nil {
-// 		fmt.Println("Not able to connect to ", connHost, "at port", connPort)
-// 		panic(err.Error())
-// 	}
-
-// 	// run loop forever, until exit.
-// 	for {
-// 		fileName := "./sample_input.txt"
-// 		file, err := os.Open(fileName)
-// 		defer file.Close()
-// 		if err != nil {
-// 			fmt.Println("Not able to read file", fileName)
-// 			break
-// 		}
-// 		scanner := bufio.NewScanner(file)
-// 		for scanner.Scan() {
-// 			var b = make([]byte, 3, 6)
-
-// 			// We add \n because scanner.Text() removes the ending newline character
-// 			conn.Write([]byte(scanner.Text() + "\n"))
-
-// 			// Wait for the server message to indicate that the line is written
-// 			conn.Read(b)
-// 		}
-// 		break
-// 	}
-// }
-
-// func getHostandPort(args []string) (string, string) {
-// 	var host string
-// 	var port string
-// 	if len(os.Args) <= 2 {
-// 		fmt.Println("Host and port not specified")
-// 		host = "localhost"
-// 		port = "8080"
-// 	} else {
-// 		host = os.Args[1]
-// 		port = os.Args[2]
-// 	}
-// 	return host, port
-// }
-
-// func ClientCheck() {
-// 	fmt.Println("Client working properly")
-// }
